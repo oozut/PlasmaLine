ab6039342072ace3777390de62271b80fc754589
Guillaume Cadot <oozut91360@gmail.com>
2016-10-11 14:25:55 +0100

    The Level package can read the different level for the species and interpret the description correctly.

--- Level/Level.py
+++ Level/Level.py
@@ -1,3 +1,6 @@
+import string
+
+
 class Levels:
     """
     Class for the Levels of a atomic species
@@ -12,20 +15,36 @@ class Levels:
         """
         f = open(name_file, 'r')  # read the file
         self.levels_obj = []  # the list for the objects
-        for line in f.readlines():
+        list_level = []
+        list_level_bool = False
+        for line in f.readlines()[3:]:
             list_line= line.split('|')
-            if not(list_line[0].strip() == list_line[3].strip()):  # Ignore the line that does not contains information
-                # about the level
-                if not(list_line[0].strip() == ''):
-                    level = Level(list_line[0].strip(), list_line[1].strip(), list_line[2].strip(), list_line[3].strip())
-                else:
-                    level = Level(self.levels_obj[-1].configuration, list_line[1].strip(), list_line[2].strip(), list_line[3].strip())
-                level.configuration_strip_down()
-                self.levels_obj.append(level)
-
-
-            #print(line.split('|'))
-
+            if not (len(list_line)== 1) and not list_line[0][0] == '-':
+                if not(list_line[0].strip() == list_line[3].strip()):  # Ignore the line that does not contains information
+                    # about the level
+                    if (list_line[3].strip() == ''):
+                        list_level.append([list_line[0].strip(), list_line[1], list_line[2].strip(),
+                                           list_line[3].strip()])
+                        list_level_bool = True
+                    else:
+                        if list_level_bool:
+                            for l in list_level:
+                                self.levels_obj.append(Level(list_level[0][0], l[1], l[2], list_line[3].strip()))
+                            self.levels_obj.append(Level(list_level[0][0], list_line[1].strip(), list_line[2].strip(),
+                                list_line[3].strip()))
+                            list_level_bool = False
+                            list_level = []
+                        else:
+                            if not(list_line[0].strip() == ''):
+                                level = Level(list_line[0].strip(), list_line[1].strip(), list_line[2].strip(),
+                                list_line[3].strip())
+                            else:
+                                level = Level(self.levels_obj[-1].configuration, list_line[1].strip(),
+                                              list_line[2].strip(),list_line[3].strip())
+                            level.configuration_strip_down()
+                            self.levels_obj.append(level)
+            else:
+                exit
 
 
 class Level:
@@ -48,7 +67,10 @@ class Level:
         self.configuration = configuration
         self.term = term
         self.J = parity
+        bad_characters = '[]()<>'  # type of characters founds in the NIST database
+        level = level.translate(None, bad_characters)
         self.level = float(level)
+
 
 
         self.configuration_high_detail = [] ## the high detail for the configuration
